c8056648124203009a3e5045b501dd3245b7fd9e
Denis Ivanov <denis.ivanov@haulmont.com>
2021-09-01 02:38:43 +0400

    Environment setup

---
+++ src/modules/app/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from 'src/assetts/svg/logo.svg';
+import 'src/modules/app/App.css';
+
+/**
+ *
+ * @constructor
+ */
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/modules/app/App.tsx</code> and save to reload.
+        </p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
