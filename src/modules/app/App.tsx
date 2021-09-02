46f301ea1f0669d0a16c0ecf96626c1606a46c6a
Denis Ivanov <denis.ivanov@haulmont.com>
2021-09-02 18:43:01 +0400

    Testing

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import logo from 'src/assetts/svg/logo.svg';
-import 'src/modules/app/App.css';
+import {Container} from 'react-bootstrap';
+import 'src/modules/app/App.scss';
+import {Header} from 'src/modules/header/Header';
+import 'src/modules/header/Header.scss';
+
 
 /**
  *
@@ -8,17 +11,9 @@ import 'src/modules/app/App.css';
  */
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/modules/app/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Header/>
+    </Container>
   );
 }
 
